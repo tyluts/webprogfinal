adce1d5f577d0f0a4310fb7186855a4c27dda06b
tyluts <cyrusballon2@gmail.com>
2024-11-11 18:25:06 +0800

    Update accordion.php

--- home/accordion.php
+++ home/accordion.php
@@ -4,42 +4,42 @@
         <div class="accordion-item dark-grey light-grey">
             <h2 class="accordion-header  ">
             <button class="accordion-button bg-dark text-white montserrat " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
+                Civil Engineering
             </button>
             </h2>
             <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                 <br>
-                <button class="btn bg-red text-white mt-2">Click Me</button>
+                <a href="programs/coe/civil.php" class="btn bg-red text-white mt-2">Click Me</a>
             </div>
             </div>
         </div>
         <div class="accordion-item dark-grey light-grey ">
             <h2 class="accordion-header">
             <button class="accordion-button collapsed bg-dark text-white montserrat" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
+                Information Technology
             </button>
             </h2>
             <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                 <br>
-                <button class="btn bg-red text-white mt-2">Click Me</button>            
+                <a  href="programs/ccs/it.php" class="btn bg-red text-white mt-2">Click Me</a>            
             </div>
             </div>
         </div>
         <div class="accordion-item dark-grey light-grey">
             <h2 class="accordion-header">
             <button class="accordion-button collapsed bg-dark text-white montserrat" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
+                Accounting
             </button>
             </h2>
             <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                 <br>
-                <button class="btn bg-red text-white mt-2">Click Me</button>            
+                <a  href="programs/cba/accountant.php" class="btn bg-red text-white mt-2">Click Me</a>            
             </div>
             </div>
         </div>
