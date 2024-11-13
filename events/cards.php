94bca28b153d04967e5e5b922372ff6d5ea1497a
danmikel1 <llenares.danmikel1@gmail.com>
2024-11-14 03:35:53 +0800

    Update cards.php

--- events/cards.php
+++ events/cards.php
@@ -32,33 +32,6 @@
           </tr>
       <?php endif; ?>
 
-        <!-- Card 2 -->
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card event-card dark-grey">
-                <img src="https://via.placeholder.com/300x200" alt="Event Image" class="event-image">
-                <span class="date-badge bg-red">11 Jan 2023</span>
-                <div class="card-body">
-                    <p class="text-white"><i class="fas fa-map-marker-alt"></i> IAC Building</p>
-                    <p class="text-white"><i class="fas fa-clock"></i> 8:00 am - 5:00 pm</p>
-                    <h5 class="card-title text-white">Elegant Light Box Paper Cut Dioramas in UK</h5>
-                     <button  href="#"  type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal"  class="btn bg-red text-white">Getticker</button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Card 3 -->
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card event-card dark-grey">
-                <img src="https://via.placeholder.com/300x200" alt="Event Image" class="event-image">
-                <span class="date-badge bg-red">11 Jan 2023</span>
-                <div class="card-body">
-                    <p class="text-white"><i class="fas fa-map-marker-alt"></i> IAC Building</p>
-                    <p class="text-white"><i class="fas fa-clock"></i> 8:00 am - 5:00 pm</p>
-                    <h5 class="card-title text-white">Elegant Light Box Paper Cut Dioramas in UK</h5>
-                     <button  href="#"  type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal"  class="btn bg-red text-white">Getticker</button>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
@@ -73,7 +46,7 @@
 
         
            var user_id = $(this).closest('div').find('.user_id').val();
-           /*console.log(user_id);*/
+          
 
            $.ajax({
 
